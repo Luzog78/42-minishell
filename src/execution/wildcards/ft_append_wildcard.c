a18a9b6be709dea4028ea1bff627e16f6eb2cf2b
Yanis Sabik <ysabik@student.42angouleme.fr>
2024-02-17 03:55:59 +0100

    Norm

--- src/execution/wildcards/ft_append_wildcard.c
+++ src/execution/wildcards/ft_append_wildcard.c
@@ -6,53 +6,53 @@
 /*   By: ysabik <ysabik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 02:36:02 by ysabik            #+#    #+#             */
-/*   Updated: 2024/02/17 02:36:17 by ysabik           ###   ########.fr       */
+/*   Updated: 2024/02/17 03:42:07 by ysabik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
-void	ft_append_wildcard(char ***args, char *str, char **env)
+static t_bool	ft_readdir(DIR *dir, char *new, t_str_lst **tmp_args)
 {
-	DIR			*dir;
-	char		*new;
-	t_bool		is_matched;
-	char		*tmp;
-	t_str_lst	*tmp_args;
+	struct dirent	*entry;
+	t_bool			matched;
+	char			*tmp;
 
-	new = ft_get_bash_string(str, env, TRUE);
-	tmp_args = NULL;
-	is_matched = FALSE;
-	dir = opendir(".");
-	if (!dir)
-	{
-		free(new);
-		free(str);
-		return ;
-	}
+	matched = FALSE;
 	while (1)
 	{
-		struct dirent	*entry;
 		entry = readdir(dir);
 		if (!entry)
 			break ;
 		if ((entry->d_name[0] != '.' || new[0] == '.')
 			&& wc_allow(entry->d_name, new))
 		{
-			is_matched = TRUE;
+			matched = TRUE;
 			tmp = ft_strdup(entry->d_name);
 			if (tmp)
-				ft_str_lst_add(&tmp_args, tmp);
+				ft_str_lst_add(tmp_args, tmp);
 		}
 	}
-	if (!is_matched)
+	return (matched);
+}
+
+void	ft_append_wildcard(char ***args, char *str, char **env)
+{
+	DIR			*dir;
+	char		*new;
+	t_str_lst	*tmp_args;
+
+	new = ft_get_bash_string(str, env, TRUE);
+	tmp_args = NULL;
+	dir = opendir(".");
+	if (dir && !ft_readdir(dir, new, &tmp_args))
 	{
 		free(new);
 		new = ft_get_bash_string(str, env, FALSE);
 		if (new)
 			ft_args_realloc(args, new);
 	}
-	else
+	else if (dir)
 	{
 		ft_sort_list(&tmp_args);
 		ft_args_reallocs(args, &tmp_args);
--- src/execution/wildcards/wc_allow_middle.c
+++ src/execution/wildcards/wc_allow_middle.c
@@ -6,11 +6,44 @@
 /*   By: ysabik <ysabik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 02:24:23 by ysabik            #+#    #+#             */
-/*   Updated: 2024/02/17 02:24:37 by ysabik           ###   ########.fr       */
+/*   Updated: 2024/02/17 03:52:40 by ysabik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
+
+static t_bool	ft_skip_start_end(t_wc_token *tk, char **file, char **tmp)
+{
+	if (tk->start)
+	{
+		*tmp = *file;
+		*file = ft_substr(*file, ft_strlen(tk->start),
+				ft_strlen(*file) - ft_strlen(tk->start));
+		free(*tmp);
+		if (!*file)
+			return (FALSE);
+	}
+	if (tk->end)
+	{
+		*tmp = *file;
+		*file = ft_substr(*file, 0, ft_strlen(*file) - ft_strlen(tk->end));
+		free(*tmp);
+		if (!*file)
+			return (FALSE);
+	}
+	return (TRUE);
+}
+
+static t_bool	ft_init_loop(t_wc_token *tk, char **file, char **tmp,
+	t_str_lst **cursor)
+{
+	*file = ft_strdup(*file);
+	if (!*file || !ft_skip_start_end(tk, file, tmp))
+		return (FALSE);
+	*tmp = *file;
+	*cursor = tk->tks;
+	return (TRUE);
+}
 
 t_bool	wc_allow_middle(t_wc_token *tk, char *file)
 {
@@ -19,28 +52,8 @@ t_bool	wc_allow_middle(t_wc_token *tk, char *file)
 
 	if (!tk->tks)
 		return (TRUE);
-	file = ft_strdup(file);
-	if (!file)
+	if (!ft_init_loop(tk, &file, &tmp, &cursor))
 		return (FALSE);
-	if (tk->start)
-	{
-		tmp = file;
-		file = ft_substr(file, ft_strlen(tk->start),
-				ft_strlen(file) - ft_strlen(tk->start));
-		free(tmp);
-		if (!file)
-			return (FALSE);
-	}
-	if (tk->end)
-	{
-		tmp = file;
-		file = ft_substr(file, 0, ft_strlen(file) - ft_strlen(tk->end));
-		free(tmp);
-		if (!file)
-			return (FALSE);
-	}
-	tmp = file;
-	cursor = tk->tks;
 	while (cursor)
 	{
 		if (!cursor->value)
@@ -50,12 +63,8 @@ t_bool	wc_allow_middle(t_wc_token *tk, char *file)
 			free(tmp);
 			return (FALSE);
 		}
-		if (ft_strncmp(file, cursor->value, ft_strlen(cursor->value)))
-		{
-			file++;
+		if (ft_strncmp(file++, cursor->value, ft_strlen(cursor->value)))
 			continue ;
-		}
-		file++;
 		cursor = cursor->next;
 	}
 	free(tmp);
