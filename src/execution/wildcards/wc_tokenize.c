4206ceabcbcbdd2ad31350766be2b9e91b96df79
Yanis Sabik <ysabik@student.42angouleme.fr>
2024-02-17 06:37:02 +0100

    Norm + .souffrance

--- src/execution/wildcards/wc_tokenize.c
+++ src/execution/wildcards/wc_tokenize.c
@@ -3,75 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   wc_tokenize.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bcarolle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ysabik <ysabik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 23:43:33 by ysabik            #+#    #+#             */
-/*   Updated: 2024/02/17 04:24:28 by bcarolle         ###   ########.fr       */
+/*   Updated: 2024/02/17 06:02:14 by ysabik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
-void	wc_tokenize(t_wc_token *tk, char *wc)
+typedef struct s_wc_tokenizer
 {
 	char	quote;
 	int		i;
 	int		j;
 	int		state;
+}	t_wc_tokenizer;
 
-	quote = 0;
-	i = 0;
-	j = 0;
-	state = 0;
-	while (wc[i])
+static void	wc_on_quote(t_wc_token *token, char *wc, t_wc_tokenizer *t)
+{
+	if (t->j > 0)
 	{
-		if ((wc[i] == '\'' || wc[i] == '"')
-			&& (!quote || quote == wc[i]))
-		{
-			if (j > 0)
-			{
-				if (state == 0)
-					tk->start = ft_substr(wc, i - j, j);
-				else
-					ft_str_lst_add_back(&tk->tks, ft_substr(wc, i - j, j));
-				j = 0;
-			}
-			state = 1;
-			if (!quote)
-				quote = wc[i];
-			else if (quote == wc[i])
-				quote = 0;
-			i++;
-		}
-		else if (wc[i] == '*' && !quote)
-		{
-			if (j > 0)
-			{
-				if (state == 0)
-					tk->start = ft_substr(wc, i - j, j);
-				else
-					ft_str_lst_add_back(&tk->tks, ft_substr(wc, i - j, j));
-				j = 0;
-			}
-			state = 1;
-			while (wc[i] == '*')
-				i++;
-		}
+		if (t->state == 0)
+			token->start = ft_substr(wc, t->i - t->j, t->j);
 		else
-		{
-			if (state == 1)
-				state = 2;
-			j++;
-			i++;
-		}
+			ft_str_lst_add_back(&token->tks, ft_substr(wc, t->i - t->j, t->j));
+		t->j = 0;
 	}
-	if (j > 0)
+	t->state = 1;
+	if (!t->quote)
+		t->quote = wc[t->i];
+	else if (t->quote == wc[t->i])
+		t->quote = 0;
+	t->i++;
+}
+
+static void	wc_on_star(t_wc_token *token, char *wc, t_wc_tokenizer *t)
+{
+	if (t->j > 0)
 	{
-		if (state == 0)
-			tk->start = ft_substr(wc, i - j, j);
+		if (t->state == 0)
+			token->start = ft_substr(wc, t->i - t->j, t->j);
 		else
-			ft_str_lst_add_back(&tk->tks, ft_substr(wc, i - j, j));
+			ft_str_lst_add_back(&token->tks, ft_substr(wc, t->i - t->j, t->j));
+		t->j = 0;
 	}
-	if (state == 2 && tk->tks)
-		tk->end = ft_str_lst_pop_back(&tk->tks);
+	t->state = 1;
+	while (wc[t->i] == '*')
+		t->i++;
+}
+
+static void	wc_on_char(t_wc_tokenizer *t)
+{
+	if (t->state == 1)
+		t->state = 2;
+	t->j++;
+	t->i++;
+}
+
+void	wc_tokenize(t_wc_token *token, char *wc)
+{
+	t_wc_tokenizer	t;
+
+	t = (t_wc_tokenizer){0};
+	while (wc[t.i])
+	{
+		if ((wc[t.i] == '\'' || wc[t.i] == '"')
+			&& (!t.quote || t.quote == wc[t.i]))
+			wc_on_quote(token, wc, &t);
+		else if (wc[t.i] == '*' && !t.quote)
+			wc_on_star(token, wc, &t);
+		else
+			wc_on_char(&t);
+	}
+	if (t.j > 0)
+	{
+		if (t.state == 0)
+			token->start = ft_substr(wc, t.i - t.j, t.j);
+		else
+			ft_str_lst_add_back(&token->tks, ft_substr(wc, t.i - t.j, t.j));
+	}
+	if (t.state == 2 && token->tks)
+		token->end = ft_str_lst_pop_back(&token->tks);
 }
